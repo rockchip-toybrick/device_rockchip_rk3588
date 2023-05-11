e35841d080b4007c6e6a645aa98a03eef4e0eba0
Luo Wei <lw@rock-chips.com>
2023-05-11 09:26:00 +0800

    car: modify some config according to other change
    Signed-off-by: Luo Wei <lw@rock-chips.com>
    Change-Id: If3e1e2aa20bb42cc38315b86b705b5654f91894e

--- rk3588m_car/rk3588m_car.mk
+++ rk3588m_car/rk3588m_car.mk
@@ -50,7 +50,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.bt.power.down=true
 PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.device.primary=DSI
 PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.device.extend=HDMI-A,eDP
 PRODUCT_PROPERTY_OVERRIDES += sys.mouse.presentation=1
-PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.reserved_plane_name=Esmart3-win0
+#PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.reserved_plane_name=Esmart3-win0
 PRODUCT_PROPERTY_OVERRIDES += vendor.hwc.env_xml_path=/vendor/etc/HwComposerEnv-multidisplay.xml
 
 # Use FUSE passthrough
