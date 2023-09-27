abff68be3a0afd2e6b40a0a34521a7b9affe6ee3
Luo Wei <lw@rock-chips.com>
2023-09-27 16:54:31 +0800

    rk3588m_car: enable evb-v22 config default
    Signed-off-by: Luo Wei <lw@rock-chips.com>
    Change-Id: I40b6dc48bfbfb2ff9001dba649a89528af1676e1

--- rk3588m_car/BoardConfig.mk
+++ rk3588m_car/BoardConfig.mk
@@ -39,8 +39,8 @@ ifeq ($(strip $(BOARD_USES_AB_IMAGE)), true)
 endif
 
 PRODUCT_UBOOT_CONFIG := rk3588-ab
-PRODUCT_KERNEL_DTS := rk3588-vehicle-evb-v20
-PRODUCT_KERNEL_CONFIG := rockchip_defconfig android-11.config sdio_wifi_cywdhd.config
+PRODUCT_KERNEL_DTS := rk3588-vehicle-evb-v22
+PRODUCT_KERNEL_CONFIG := rockchip_defconfig android-11.config pcie_wifi_infineon.config
 PRODUCT_KERNEL_CONFIG += rk3588_vehicle.config
 BOARD_GSENSOR_MXC6655XA_SUPPORT := true
 BOARD_CAMERA_SUPPORT_EXT := true
